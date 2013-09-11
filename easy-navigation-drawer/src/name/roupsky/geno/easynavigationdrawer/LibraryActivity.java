3573b6d8b3312b83c51912a61f4bb108e23b108e
Geno Roupsky <geno@roupsky.name>
2013-09-11 22:36:56 +0300

    renamed projects

--- easy-navigation-drawer/src/name/roupsky/geno/easynavigationdrawer/LibraryActivity.java
+++ easy-navigation-drawer/src/name/roupsky/geno/easynavigationdrawer/LibraryActivity.java
@@ -1,8 +1,9 @@
 
-package name.roupsky.geno.library;
+package name.roupsky.geno.easynavigationdrawer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import name.roupsky.geno.easynavigationdrawer.R;
 
 public class LibraryActivity extends Activity {
 
